d6160ff72834b64941a7d75e4c4dd8b16a183309
Cecile Hannay <hannay@ucar.edu>
2021-03-05 11:54:00 -0700

    Parsing select time-series

---
+++ read_text_file_into_dictionary.py
@@ -0,0 +1,30 @@
+# This script the environment: conda activate python_tutorial
+
+# Initialize data variable
+datatable = {'date': [ ],
+    'time':[],
+    'temperature': []}
+
+
+# open the data file
+filename = "data/wxobs20170821.txt"
+datafile = open(filename, "r")
+
+# skip the 3 first lines
+# Because the variable _ is never called in the loop we just call it: _
+for _ in range(3):
+    datafile.readline()
+
+# read line by line
+# Use the concept of split() and append()
+for line in datafile:
+    row = line.split()
+    datatable['date'].append(row[0])
+    datatable['time'].append(row[1])
+    datatable['temperature'].append(row[2])
+
+# print for debug
+print(datatable['temperature'])
+
+# close file
+datafile.close()
